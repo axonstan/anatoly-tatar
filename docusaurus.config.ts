abdee8f39068468af3fcc5e589b64178f2d6f7c0
PodYapolskiy <podyapolskiyaa@gmail.com>
2025-09-06 20:32:49 +0300

    feat: make docs left sidebar hideable

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -156,6 +156,11 @@ const config: Config = {
         },
       ],
     },
+    docs: {
+      sidebar: {
+        hideable: true
+      }
+    },
     // footer: {
     //   style: 'light',
     //   links: [
