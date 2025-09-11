16d065d06b01386bb636de64e38e075d34573cb4
PodYapolskiy <podyapolskiyaa@gmail.com>
2025-09-11 22:24:00 +0300

    feat: update about page with content and its russian translations

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -38,19 +38,19 @@ export default function About(): ReactNode {
   });
 
   const sidebar = [
-    { title: "Introduction", link: "#introduction", depth: 1 },
+    { title: translate({ id: "about.introduction", message: "Introduction" }), link: "#introduction", depth: 1 },
     // Work Experience
-    { title: "Work Experience", link: "#work-experience", depth: 1 },
-    { title: "MTS Web Services", link: "#mts-web-services", depth: 2 },
-    { title: "AIRI", link: "#airi", depth: 2 },
-    { title: "Along", link: "#along", depth: 2 },
+    { title: translate({ id: "about.work-experience", message: "Work Experience" }), link: "#work-experience", depth: 1 },
+    { title: translate({ id: "about.work-experience.mts-web-services", message: "MTS Web Services" }), link: "#mts-web-services", depth: 2 },
+    { title: translate({ id: "about.work-experience.airi", message: "AIRI" }), link: "#airi", depth: 2 },
+    { title: translate({ id: "about.work-experience.along", message: "Along" }), link: "#along", depth: 2 },
     // Pet Projects
     // { title: "Pet Projects", link: "#pet-projects", depth: 1 },
     // { title: "CV Project", link: "#cv-project", depth: 2 },
     // Studies
-    { title: "Studies", link: "#studies", depth: 1 },
-    { title: "Innopolis University", link: "#innopolis-university", depth: 2 },
-    { title: "School", link: "#school", depth: 2 },
+    { title: translate({ id: "about.studies", message: "Studies" }), link: "#studies", depth: 1 },
+    { title: translate({ id: "about.studies.innopolis-university", message: "Innopolis University" }), link: "#innopolis-university", depth: 2 },
+    { title: translate({ id: "about.studies.school", message: "School" }), link: "#school", depth: 2 },
   ];
 
   return (
@@ -88,61 +88,52 @@ export default function About(): ReactNode {
           ) : null}
           <div className='col col--7' >
             <main>
-              <h1>Anatoly Soldatov</h1>
-              <h4>ML Engineer</h4>
+              <h1>{translate({ id: "global.title", message: "Anatoly Soldatov" })}</h1>
+              <h4>{translate({ id: "global.tagline", message: "ML Engineer" })}</h4>
               <section style={{ marginTop: '3em' }}>
-                <h2 id="introduction">Introduction</h2>
+                <h2 id="introduction">{translate({ id: "about.introduction", message: "Introduction" })}</h2>
                 <p>
-                  I'm Anatoly, ML Engineer, Innopolis University bachelor student. I'm eager to learn about programming, machine learning, foreign languages, etc.
+                  {translate({ id: "about.introduction.description", message: "I'm Anatoly, ML Engineer, Innopolis University bachelor student. I'm eager to learn about programming, machine learning, foreign languages, etc." })}
                 </p>
               </section>
 
               <section style={{ marginTop: '3em' }}>
-                <h2 id="work-experience">Work Experience</h2>
+                <h2 id="work-experience">{translate({ id: "about.work-experience", message: "Work Experience" })}</h2>
                 <section>
                   <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'top' }}>
-                    <h3 id='mts-web-services'>MTS Web Services</h3>
-                    <p style={{ fontStyle: 'italic', margin: 0 }}>06.2025 - to present</p>
+                    <h3 id='mts-web-services'>{translate({ id: "about.work-experience.mts-web-services", message: "MTS Web Services" })}</h3>
+                    <p style={{ fontStyle: 'italic', margin: 0 }}>06.2025 - {translate({ id: "about.work-experience.to-present", message: "to present" })}</p>
                   </div>
-                  <h4 className={styles.aboutRoleText}>ML Engineer</h4>
+                  <h4 className={styles.aboutRoleText}>{translate({ id: "about.work-experience.mts-web-services.role", message: "ML Engineer" })}</h4>
                   <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    {/* <li>Designed and developed a personal portfolio website using Next.js and Tailwind CSS.</li>
-                    <li>Implemented a blog section with Markdown support for easy content management.</li>
-                    <li>Deployed the website on Vercel for seamless hosting and continuous integration.</li> */}
+                    <li>{translate({ id: "about.work-experience.mts-web-services.1", message: "Added the functionality of the RAG service on FastAPI, storing about 1000 processed corporate documents in Qdrant for vector search" })}</li>
+                    <li>{translate({ id: "about.work-experience.mts-web-services.2", message: "I have set up monitoring of 25 metrics for vLLM and RAG service for tracking in the product through Prometheus and Grafana" })}</li>
+                    <li>{translate({ id: "about.work-experience.mts-web-services.3", message: "I have configured the measurement of 4 LLM-as-a-Judge RAGAS metrics to evaluate the performance of the RAG pipeline and conduct further comparative experiments using Langfuse" })}</li>
                   </ul>
                 </section>
 
                 <section>
                   <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'top' }}>
-                    <h3 id='airi'>AIRI</h3>
+                    <h3 id='airi'>{translate({ id: "about.work-experience.airi", message: "AIRI" })}</h3>
                     <p style={{ fontStyle: 'italic', margin: 0 }}>03.2025 - 06.2025</p>
                   </div>
-                  <h4 className={styles.aboutRoleText}>ML Practitioner</h4>
+                  <h4 className={styles.aboutRoleText}>{translate({ id: "about.work-experience.airi.role", message: "ML Practitioner" })}</h4>
                   <ul>
-                    <li>Занял #airi-number[7]/11 и #airi-number[5]/9 места на personality и performance треках соревнования #link("https://avichallenge.github.io/challenge.html",[#term[AVI Challenge 2025]])</li>
-                    <li>[Прогнал более #airi-number[100] сравнительных экспериментов сочетания модальностей и архитектур моделей, отслеживая обучение и метрики через #term[MLflow]</li>
-                    {/* <li>Designed and developed a personal portfolio website using Next.js and Tailwind CSS.</li>
-                    <li>Implemented a blog section with Markdown support for easy content management.</li>
-                    <li>Deployed the website on Vercel for seamless hosting and continuous integration.</li> */}
+                    <li>{translate({ id: "about.work-experience.airi.1", message: "I took 7th/11th and 5th/9th places on the personality and performance tracks of the AVI Challenge 2025 competition" })}</li>
+                    <li>{translate({ id: "about.work-experience.airi.2", message: "I ran more than 100 comparative experiments combining modalities and model architectures, tracking training and metrics through MLflow" })}</li>
                   </ul>
                 </section>
 
                 <section>
                   <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'top' }}>
-                    <h3 id='along'>Along</h3>
+                    <h3 id='along'>{translate({ id: "about.work-experience.along", message: "Along" })}</h3>
                     <p style={{ fontStyle: 'italic', margin: 0 }}>01.2025 - 03.2025</p>
                   </div>
-                  <h4 className={styles.aboutRoleText}>Prompt Engineer</h4>
+                  <h4 className={styles.aboutRoleText}>{translate({ id: "about.work-experience.along.role", message: "Prompt Engineer" })}</h4>
                   <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    {/* <li>Designed and developed a personal portfolio website using Next.js and Tailwind CSS.</li>
-                    <li>Implemented a blog section with Markdown support for easy content management.</li>
-                    <li>Deployed the website on Vercel for seamless hosting and continuous integration.</li> */}
+                    <li>{translate({ id: "about.work-experience.along.1", message: "Set up 3 pipelines with business logic in n8n" })}</li>
+                    <li>{translate({ id: "about.work-experience.along.2", message: "Optimized the prompts of the OpenAI analyze image node for the taxonomy and tasks of the business domain, increasing the accuracy of recognition of classes and subclasses of the domain to 90%" })}</li>
+                    <li>{translate({ id: "about.work-experience.along.3", message: "Improved the quality of the OpenAI edit image node, for stable generation of isolated objects with a transparent background, reducing the consumption of tokens by 2 times" })}</li>
                   </ul>
                 </section>
               </section>
@@ -177,25 +168,19 @@ export default function About(): ReactNode {
               </section> */}
 
               <section style={{ marginTop: '3em' }}>
-                <h2 id="studies">Studies</h2>
+                <h2 id="studies">{translate({ id: "about.studies", message: "Studies" })}</h2>
                 <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'top' }}>
-                  <h3 id="innopolis-university">Innopolis University</h3>
+                  <h3 id="innopolis-university">{translate({ id: "about.studies.innopolis-university", message: "Innopolis University" })}</h3>
                   <p style={{ fontStyle: 'italic', margin: 0 }}>2022 - 2026</p>
                 </div>
-                <p>Applied Artificial Intelligence Bachelor</p>
-                {/* <ul>
-                  <li>Obtained a Bachelor's degree in Computer Science.</li>
-                  <li>Completed coursework in Data Structures, Algorithms, and Web Development.</li>
-                  <li>Participated in hackathons and coding competitions, achieving top placements.</li>
-                </ul> */}
+                <p>{translate({ id: "about.studies.innopolis-university.description", message: "Applied Artificial Intelligence Bachelor" })}</p>
 
                 <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'top' }}>
-                  <h3 id="school">School</h3>
+                  <h3 id="school">{translate({ id: "about.studies.school", message: "School" })}</h3>
                   <p style={{ fontStyle: 'italic', margin: 0 }}>2011 - 2022</p>
                 </div>
-                <p>Moscow №1288</p>
+                <p>{translate({ id: "about.studies.school.description", message: "Moscow №1288" })}</p>
               </section>
-
             </main>
           </div>
         </div>
